3ee4a00c7268321c212cd715631b672a70a19f9e
Maxim Korzenok <juzete.pgm@gmail.com>
2023-05-08 23:09:35 +0300

    feat: add storage persistense

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import matchesReducer from './slices/matchesSlice'
 import playersReducer from './slices/playersSlice'
 import transactionsReducer from './slices/transactionsSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const persistConfig = {
+  key:  'root',
+  storage,
+}
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//     matches: matchesReducer,
+//     players: playersReducer,
+//     transactions: transactionsReducer,
+//   },
+// })
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  matches: matchesReducer,
+  players: playersReducer,
+  transactions: transactionsReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    matches: matchesReducer,
-    players: playersReducer,
-    transactions: transactionsReducer,
-  },
+  reducer: persistedReducer,
 })
+
+export const persistor = persistStore(store)
